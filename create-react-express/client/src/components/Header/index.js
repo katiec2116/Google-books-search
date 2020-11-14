1a3adf2fc6c77d6a92625a98bd6592c69c594179
katiec <katiecampbell21@outlook.com>
2020-11-14 09:13:30 -0500

    changed to jumbotron

---
+++ create-react-express/client/src/components/Header/index.js
@@ -0,0 +1,15 @@
+import React from "react"
+function Header() {
+    return (
+        <div>
+            <div className="jumbotron text-center">
+                <div className="container">
+                        <h1>Google Books Search</h1>
+                        <p style={{fontSize:"20px"}}>Search for and save books of interest</p>
+                </div>
+            </div>
+        </div>
+    )
+};
+
+export default Header
