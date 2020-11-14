526e37978008cf3db114ba7b26c64edcce87e763
katiec <katiecampbell21@outlook.com>
2020-11-14 07:43:05 -0500

    paths to pages"

--- create-react-express/client/src/App.js
+++ create-react-express/client/src/App.js
@@ -1,21 +1,24 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react"
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Nav from "./components/Nav";
+import Header from "./components/Header"
+import Home from "./pages/Home"
+import NoMatch from "./pages/NoMatch"
+import 'bulma/css/bulma.css'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+function App() {
+  return (
+
+    <Router>
+      <Nav />
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        {/* <Route exact path="/saved" component={Saved} /> */}
+        <Route component={NoMatch} />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
